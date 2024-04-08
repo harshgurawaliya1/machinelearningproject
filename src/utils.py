485f3fe1bb109fc3a8c160ba3c8d0bf373a08abd
harshgurawaliya1 <harsh.gurawaliya@stud.th-deg.de>
2024-04-08 12:15:58 +0200

    Added Hyper parameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     
@@ -23,16 +23,24 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
             
             
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+
+            
 
             #model.fit(X_train, y_train)  # Train model
 
